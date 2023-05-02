0dc6119a051eaacae8ae6b96918ae3f7f1e20460
Christian Preston <christianpreston@ymail.com>
2023-05-02 12:05:11 -0400

    chore: fix

--- formkit.config.ts
+++ formkit.config.ts
@@ -3,7 +3,7 @@ import { createAutoAnimatePlugin } from '@formkit/addons'
 import { DefaultConfigOptions as FormKitConfig } from '@formkit/vue'
 
 // Custom Input
-import FKTurnstile from '~/components/FormKit/FKTurnstile.vue';
+import FKTurnstile from './components/FormKit/FKTurnstile.vue';
 
 const config: FormKitConfig = {
   inputs: {
